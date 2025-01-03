23e3a4d1589d25b949cffeb71df6ab205379b977
Ryley Watson <ryleyswatson@gmail.com>
2025-01-03 09:47:49 -0800

    Completes Lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+function moveDodgerLeft() {
+    let left = dodger.style.left.replace("px", "");
+    left = parseInt(left, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  function moveDodgerRight() {
+    let left = dodger.style.left.replace("px", "");
+    left = parseInt(left, 10);
+  
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
